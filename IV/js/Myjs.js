605a73781e225ba5fff2894fa4614e2c3eedff1d
agates4 <agate97@gmail.com>
2018-05-11 17:21:50 -0400

    updated changes to interact with specific dataset

--- IV/js/Myjs.js
+++ IV/js/Myjs.js
@@ -123,6 +123,8 @@ map.on('draw:created', function (e) {
             });
 
             getStreetWords(streetnames);
+            console.log(d);
+            getScatterPlot(d);
 
             // console.log(result);		// Trip Info: avspeed, distance, duration, endtime, maxspeed, minspeed, starttime, streetnames, taxiid, tripid
             DrawRS(result);
@@ -154,6 +156,140 @@ function DrawRS(trips) {
 	}		
 }
 
+        var width = 300,
+        size = 150,
+        padding = 20;
+
+    var x = d3.scale.linear()
+        .range([padding / 2, size - padding / 2]);
+
+    var y = d3.scale.linear()
+        .range([size - padding / 2, padding / 2]);
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("top")
+        .ticks(6);
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("right")
+        .ticks(6);
+
+    var color = d3.scale.category10();
+
+function getScatterPlot(fullData) {
+    var data = [];
+        for (var key in fullData) {
+            data.push({
+                speed: fullData[key].properties.avspeed,
+                distance: fullData[key].properties.distance,
+                duration: fullData[key].properties.duration
+            });
+        }
+
+        console.log(data)
+
+        var domainTrait = {},
+            allAtrributes = d3.keys(data[0]),
+            n = allAtrributes.length;
+
+        allAtrributes.forEach(function(trait) {
+            domainTrait[trait] = d3.extent(data, function(d) {
+                return d[trait];
+            });
+        });
+
+        xAxis.tickSize(size * n);
+        yAxis.tickSize(-size * n);
+
+        var svg = d3.select("#rightside").append("svg")
+            .attr("width", size * n + padding)
+            .attr("height", size * n + padding)
+            .append("g")
+            .attr("transform", "translate(" + padding + "," + padding / 2 + ")");
+
+        svg.selectAll(".x.axis")
+            .data(allAtrributes)
+            .enter().append("g")
+            .attr("class", "x axis")
+            .attr("transform", function(d, i) {
+                return "translate(" + (n - i - 1) * size + ",0)";
+            })
+            .each(function(d) {
+                x.domain(domainTrait[d]);
+                d3.select(this).call(xAxis);
+            });
+
+        svg.selectAll(".y.axis")
+            .data(allAtrributes)
+            .enter().append("g")
+            .attr("class", "y axis")
+            .attr("transform", function(d, i) {
+                return "translate(0," + i * size + ")";
+            })
+            .each(function(d) {
+                y.domain(domainTrait[d]);
+                d3.select(this).call(yAxis);
+            });
+
+        var cell = svg.selectAll(".cell")
+            .data(cross(allAtrributes, allAtrributes))
+            .enter().append("g")
+            .attr("class", "cell")
+            .attr("transform", function(d) {
+                return "translate(" + (n - d.i - 1) * size + "," + d.j * size + ")";
+            })
+            .each(plot);
+
+            function plot(p) {
+                var cell = d3.select(this);
+            
+                x.domain(domainTrait[p.x]);
+                y.domain(domainTrait[p.y]);
+            
+                cell.append("rect")
+                    .attr("class", "frame")
+                    .attr("width", size - padding)
+                    .attr("height", size - padding)
+                    .attr("x", padding / 2)
+                    .attr("y", padding / 2);
+                cell.selectAll("circle")
+                    .data(data)
+                    .enter().append("circle")
+                    .attr("cx", function(d) {
+                        return x(d[p.x]);
+                    })
+                    .attr("cy", function(d) {
+                        return y(d[p.y]);
+                    })
+                    .attr("r", 4)
+                    .style("fill", function(d) {
+                        return color(d.duration);
+                    }).on("click", function(d){
+                        alert("Distance = " + Math.round(d.distance) + "\n" +
+                        "Speed = " + Math.round(d.speed) + "\n" +
+                        "Duration = " + Math.round(d.duration) + "\n")
+                    })
+            }
+            
+            function cross(a, b) {
+                var c = [],
+                    n = a.length,
+                    m = b.length,
+                    i, j;
+                for (i = -1; ++i < n;)
+                    for (j = -1; ++j < m;) c.push({
+                        x: a[i],
+                        i: i,
+                        y: b[j],
+                        j: j
+                    });
+                return c;
+            }
+    
+}
+
 function getStreetWords(result) {
     var counts = {};
     for (var i = 0; i < result.length; i++) {
@@ -167,5 +303,42 @@ function getStreetWords(result) {
                 counts[element] = 1;
         }
     }
-    console.log(counts);
+    frequency_list = []
+    for (var key in counts) {
+        frequency_list.push({"text":key,"size":counts[key] / 50 < 1 ? 0 : counts[key] / 50})
+        // $('#chart1').append("<text style=\"font-size:" + counts[key] + ";\">" + key + "</text>")
+        // console.log(key, counts[key]);
+    }
+    var color = d3.scale.linear()
+            .domain([0,1,2,3,4,5,6,10,15,20,100])
+            .range(["#ddd", "#ccc", "#bbb", "#aaa", "#999", "#888", "#777", "#666", "#555", "#444", "#333", "#222"]);
+
+    d3.layout.cloud().size([850, 350])
+            .words(frequency_list)
+            .rotate(0)
+            .fontSize(function(d) { return d.size; })
+            .on("end", draw)
+            .start();
+
+    function draw(words) {
+        d3.select("#rightside").append("svg")
+                .attr("width", 850)
+                .attr("height", 350)
+                .attr("class", "wordcloud")
+                .append("g")
+                // without the transform, words words would get cutoff to the left and top, they would
+                // appear outside of the SVG area
+                .attr("transform", "translate(320,200)")
+                .selectAll("text")
+                .data(words)
+                .enter().append("text")
+                .style("font-size", function(d) { return d.size + "px"; })
+                .style("fill", function(d, i) { return color(i); })
+                .attr("transform", function(d) {
+                    return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+                }).on("click", function(d){
+                    alert("frequency: " + counts[d.text])
+                })
+                .text(function(d) { return d.text; });
+    }
 }
